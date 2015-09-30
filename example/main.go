88551e406aecb5b2e4b622ed37773e6d008212a6
shpaker <shpaker@gmail.com>
2015-09-30 20:29:53 +0600

    fix import, adding flags

--- example/main.go
+++ example/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"github.com/shpaker/pogoda"
+
+	"flag"
 	"fmt"
 	"log"
-	"pogoda"
 )
 
 func main() {
+	var cityFlag string
+	flag.StringVar(&cityFlag, "c", "Новосибирск", "Город")
+	flag.Parse()
 	countries, err := pogoda.NewCountries()
 	if err != nil {
 		log.Fatal(err)
 	}
-	cities, err := countries.GetCities("Барнаул")
+	cities, err := countries.GetCities(cityFlag)
 	if err != nil {
 		log.Fatal(err)
 	}
 	var forecast *pogoda.Fact
 	pogoda.GetForecast(cities[0].Id, &forecast)
-	fmt.Printf("%s, %s, %s (%s)\n", cities[0].Country, cities[0].Part, cities[0].Name, forecast.Fact.WeatherType)
-	fmt.Printf("Температура: %g°C \n", forecast.Fact.Temperature.Value)
-	fmt.Printf("Ветер: %g м/с Направление: %s \n", forecast.Fact.WindSpeed, forecast.Fact.WindDirection)
-	fmt.Printf("Влажность: %d%% \n", forecast.Fact.Humidity)
-	fmt.Printf("Давление: %d мм рт. ст.", forecast.Fact.Pressure.Value)
+	fmt.Printf(" %s, %s, %s (%s)\n", cities[0].Country, cities[0].Part, cities[0].Name, forecast.Fact.WeatherType)
+	fmt.Printf("  Температура: %g°C \n", forecast.Fact.Temperature.Value)
+	fmt.Printf("  Ветер: %g м/с Направление: %s \n", forecast.Fact.WindSpeed, forecast.Fact.WindDirection)
+	fmt.Printf("  Влажность: %d%% \n", forecast.Fact.Humidity)
+	fmt.Printf("  Давление: %d мм рт. ст. \n", forecast.Fact.Pressure.Value)
 }
