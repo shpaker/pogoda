27d97e25898049a5bb01334bc2fa5bef21ff0f33
shpaker <shpaker@gmail.com>
2015-10-01 15:42:27 +0600

    adding comments

--- day.go
+++ day.go
@@ -1,31 +1,27 @@
 package pogoda
 
+// Структура Day описывает данные для десериализации прогноза на день
+// Яндекс предоставляет данные на 10 дней включая текущий
 type (
 	Day struct {
-		Date      *Time      `xml:"date,attr"`
-		Sunrise   *Time      `xml:"sunrise"`
-		Sunset    *Time      `xml:"sunset"`
-		MoonPhase *MoonPhase `xml:"moon_phase"`
-		Moonrise  *Time      `xml:"moonrise"`
-		Moonset   *Time      `xml:"moonset"`
-		Biomet    *Biomet    `xml:"biomet"`
-		DayParts  []*DayPart `xml:"day_part"`
-		Hours     []*Hour    `xml:"hour"`
-	}
-
-	MoonPhase struct {
-		Value int    `xml:",chardata"`
-		Code  string `xml:"code,attr"`
-	}
-
-	Biomet struct {
-		Index    int             `xml:"index,attr"`
-		Geomag   int             `xml:"geomag,attr"`
-		Uv       int             `xml:"uv,attr"`
-		Messages []BiometMessage `xml:"message"`
-	}
-
-	BiometMessage struct {
-		Code string `xml:"code,attr"`
+		Date      *Time `xml:"date,attr"` // Дата прогноза
+		Sunrise   *Time `xml:"sunrise"`   // Время восхода солнца
+		Sunset    *Time `xml:"sunset"`    // Время заката солнца
+		Moonrise  *Time `xml:"moonrise"`  // Время появления луны
+		Moonset   *Time `xml:"moonset"`   // Время исчезновения луны
+		MoonPhase *struct {
+			Value int    `xml:",chardata"`
+			Code  string `xml:"code,attr"`
+		} `xml:"moon_phase"`
+		Biomet *struct {
+			Index    int `xml:"index,attr"`
+			Geomag   int `xml:"geomag,attr"`
+			Uv       int `xml:"uv,attr"`
+			Messages []struct {
+				Code string `xml:"code,attr"`
+			} `xml:"message"`
+		} `xml:"biomet"`
+		DayParts []*DayPart `xml:"day_part"` // Прогноз на части дня (утро, деньб вечер, ночь)
+		Hours    []*Hour    `xml:"hour"`     // Почасовой прогноз
 	}
 )
